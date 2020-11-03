d05f789efdc917cbbd0b36e6d80eb64ca021ab14
Rico Schwab <rico-schwab@hotmail.com>
2020-11-03 01:22:13 +0100

    Some last fixes

--- static/js/main.js
+++ static/js/main.js
@@ -9,3 +9,16 @@ $('.linkButton').click(function(e) {
         }, 500);
     }
 });
+
+$('.startAnimButton').click(function(e) {
+    $(".container").removeClass("animate__fadeIn");
+    $(".container").toggleClass("animate__fadeOut");
+    e.preventDefault();
+    if (this.href) {
+        var target = this.href;
+        setTimeout(function(){
+            window.location = target;
+        }, 500);
+    }
+});
+
